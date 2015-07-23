e741322011c9cd0790da6b9bef7e4fc950f46b3a
Robert Starbuck <robstarbuck@gmail.com>
2015-07-23 22:15:11 +0100

    A version approaching production of how the site will work alongside Apache

--- node/app.js
+++ node/app.js
@@ -1,3 +1,6 @@
+// http://stackoverflow.com/questions/28277094/req-session-passport-and-req-user-empty-serializeuser-and-deserializeuser-are-n
+// http://stackoverflow.com/questions/24477035/express-4-0-express-session-with-odd-warning-message
+// https://gist.github.com/stagas/754303
 var express = require('express')
 , passport = require('passport')
 , sessions = require('express-session')
---
+++ reference.md
@@ -0,0 +1,6 @@
+
+
+- http://stackoverflow.com/questions/28277094/req-session-passport-and-req-user-empty-serializeuser-and-deserializeuser-are-n
+- http://stackoverflow.com/questions/24477035/express-4-0-express-session-with-odd-warning-message
+- https://gist.github.com/stagas/754303
+- http://arguments.callee.info/2010/04/20/running-apache-and-node-js-together/
